0aded79957aee647524590f91d58820098ac7e28
Kuldeep_Rajdev <kuldeep_rajdev@epam.com>
2021-08-12 01:33:01 +0530

    POST, DELETE & PUT Requests - Home Task -2

---
+++ src/test/java/com/apitask/hometasks/UserValidations.java
@@ -0,0 +1,21 @@
+package com.apitask.hometasks;
+
+import Resource.Data.PayloadsAndEndPoint;
+import Resource.Utility.ValidationMethod;
+import io.restassured.response.Response;
+import org.apache.http.HttpStatus;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class UserValidations {
+    PayloadsAndEndPoint peData= new PayloadsAndEndPoint();
+
+    @Test
+    public void validateUser() {
+        Response response = ValidationMethod.getResource(peData.baseURI, peData.usersEndPoint);
+        ValidationMethod.validateStatusCode(response, HttpStatus.SC_OK);
+        Assert.assertTrue(response.jsonPath().getList("id").size()>=3, "There are not 3 or More user available");
+        Assert.assertTrue(ValidationMethod.getSpecificPathValue(response, "name").contains("Ervin Howell"), "User Ervin Howell is not not present");
+        System.out.println("Validation is completed");
+    }
+}
